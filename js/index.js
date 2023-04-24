c89678f22ee88b814241a01c194afc543ca18296
TatyanaZelenina <118557910+TatyanaZelenina@users.noreply.github.com>
2023-04-24 10:13:50 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-//  Найтинужныеузлы
-constpopupButton = document.querySelector('.popup-menu_buton');
-constpopupWrapper = document.querySelector('.popup-menu');
-constpopupMenu = document.querySelector('.popup-menu_list');
+//  Найти нужные узлы
+const popupButton = document.querySelector('.popup-menu_buton');
+const popupWrapper = document.querySelector('.popup-menu');
+const popupMenu = document.querySelector('.popup-menu_list');
 
 // Выполним ф-цию клика по кнопке
 
 popupButton.addEventListener('click', handleMenu);
 
 // Ф-ция, которая показывает и скрывает поп-ап
-functionhandleMenu() {
+function handleMenu() {
     popupMenu.classList.toggle('hide-popup');
 }
 
 // Ф-ция при клике на любое место экрана
 document.addEventListener('click', hidePopup);
 
-functionhidePopup(el) {
+function hidePopup(el) {
     // Проверяем, есть и внутри того, по чему кликнули поп-ап меню или кнопка
-    lettargetInside = popupButton.contains(el.target);
+    let targetInside = popupWrapper.contains(el.target);
     // Если нет, то прячем меню
     if(!targetInside) {
         popupMenu.classList.add('hide-popup');
